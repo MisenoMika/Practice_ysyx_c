4ac434419c4491bfbc15476235fa59a7ddeb0bec
SUAT25000185-Chuntian Deng <chuntiandeng@gmail.com>
2026-02-01 17:50:22 +0800

    modify array_arrange_2.c to improve performance and fix bugs.

--- array/array_arrange_2.c
+++ array/array_arrange_2.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 int N, M;
-int path[100];
 
-void dfs(int start, int depth)
-{
-    // 终止条件：已经选了 M 个
-    if (depth == M) {
-        for (int i = 0; i < M; i++) {
-            printf("%d ", path[i]);
+int* nums;
+int* nums_index;
+void dfs(int start, int depth){
+    if(depth == M){
+        for(int i=0; i<depth; i++){
+            printf("%d  ", nums[nums_index[i]]);
         }
         printf("\n");
-        return;
     }
-
-    // 从 start 开始选
-    for (int i = start; i <= N; i++) {
-        path[depth] = i;
-        dfs(i + 1, depth + 1);  // 只能选后面的数
+    else {
+        for(int i= start; i<N; i++){
+            nums_index[depth] = i;
+            dfs(i+1, depth+1);
+        }
     }
 }
 
-int main(void)
-{
-    N = 5;
-    M = 3;
-    dfs(1, 0);
+int main(void){
+    N = 3;
+    M = 2;
+    nums = malloc(N * sizeof *nums);
+    if (!nums)
+        goto out;
+
+    nums_index = malloc(M * sizeof *nums_index);
+    if (!nums_index)
+        goto free_nums;
+
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        nums[i] = rand() % 30;
+        nums_index[i] = i;
+        printf("%d  ", nums[i]);
+    }
+    printf("\n");
+    dfs(0, 0);
+
+free_nums_index:
+    free(nums_index);
+free_nums:
+    free(nums);
+out:
     return 0;
 }
