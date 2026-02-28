83051d595648c7ac7a6fbdc690fbeeee900e35e7
SUAT25000185-Chuntian Deng <chuntiandeng@gmail.com>
2026-02-28 22:13:10 +0800

    feat: add my exercises of C language

---
+++ learn_C_hd_way/ex1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world");
+    fflush(stdout);
+    while (1);
+}
---
+++ learn_C_hd_way/ex6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char s[] = "";
+    printf("h%sl\n", s);
+    printf("%#o\n", 24);
+    return EXIT_SUCCESS;
+}
